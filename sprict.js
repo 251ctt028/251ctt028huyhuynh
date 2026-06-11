20bb27eb223073a3e8ffd8c9aaee644e96590fc1
251ctt028 <251ctt028_huynh@pyu.edu.vn>
2026-06-11 08:51:43 +0700

    Add files via upload

---
+++ sprict.js
@@ -0,0 +1,9 @@
+
+function hienThi(ten, gia){
+    document.getElementById("tenPhong").innerHTML = ten;
+    document.getElementById("giaPhong").innerHTML = gia;
+}
+
+function chonMuc(ten){
+    document.getElementById("mucchon").innerHTML = ten;
+}
